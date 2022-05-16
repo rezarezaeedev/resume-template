8e99b05a3083463b758ecdaeadd1c34e61b26225
reza rezaee <r871017h800501@gmail.com>
2022-05-17 02:15:39 +0430

    first commit - add poppins font to tailwind fonts

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontWeight: {
+      'extralight':100,
+      'light':200,
+      'normal':400,
+      'semibold':600,
+      'bold':700,
+      'bolder':900,
+    },
+    extend: {
+      
+       fontFamily:{
+         'pop':['Poppins', 'monospace']
+       },
+       
+    }
+  },
+  plugins: [],
+}
