de1fb6cc66fb03c84712bb22c22a8faee1560913
reza rezaee <r871017h800501@gmail.com>
2022-05-17 04:44:21 +0430

    completed header

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,12 @@ module.exports = {
       'bolder':900,
     },
     extend: {
-      
+      colors:{
+        'rezablack':'#343A40',
+        'rezawhite':'#F0F0F0',
+        'rezaprimary':'#FFAF40',
+        'rezasecondary':'#7B2CFF'
+      },
        fontFamily:{
          'pop':['Poppins', 'system-ui', 'sans-serif']
        },
