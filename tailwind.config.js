13680752b3316d15b353aea0462d741366e43e9e
reza rezaee <r871017h800501@gmail.com>
2022-05-20 20:32:19 +0430

    completed skills box

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,13 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     screens: {
-      'phone': '640px',
+      'phone': '640px', // => @media (min-width: 640px) { ... }
 
-      'tablet': '768px',
+      'tablet': '768px', // => @media (min-width: 768px) { ... }
       
-      'laptop': '1024px',
+      'laptop': '1024px', // => @media (min-width: 1024px) { ... }
 
-      'desktop': '1280px',
+      'desktop': '1280px', // => @media (min-width: 1280px) { ... }
     },
     fontWeight: {
       'extralight':100,
